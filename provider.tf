3a6a24c5413f4f0584dc6450ea9b4d7ab69cc65c
Jennings <jtreutel+github@protonmail.com>
2022-06-09 10:45:17 +0900

    terraform fmt

--- provider.tf
+++ provider.tf
@@ -1,12 +1,12 @@
 provider "google" {
   credentials = "key.json"
-  project = var.gcp_project_name
-  region  = var.gcp_region
+  project     = var.gcp_project_name
+  region      = var.gcp_region
 }
 
 # Required for workload identity pool resources
 provider "google-beta" {
   credentials = "key.json"
-  project = var.gcp_project_name
-  region  = var.gcp_region
+  project     = var.gcp_project_name
+  region      = var.gcp_region
 }
