2bddd77aef40c259a9669a45965acb1e7d9ad9cc
jtreutel <jtreutel@users.noreply.github.com>
2024-03-26 13:47:22 +0900

    tf fmt

--- data.tf
+++ data.tf
@@ -5,7 +5,7 @@ data "google_project" "project" {
 locals {
   sa_impersonation_filter_attribute = var.sa_impersonation_filter_attribute == "" ? "attribute.org_id" : var.sa_impersonation_filter_attribute
   sa_impersonation_filter_value     = var.sa_impersonation_filter_value == "" ? var.circleci_org_id : var.sa_impersonation_filter_value
-  project_id = data.google_project.project.project_id
+  project_id                        = data.google_project.project.project_id
 }
 
 data "google_service_account" "existing_sa" {
